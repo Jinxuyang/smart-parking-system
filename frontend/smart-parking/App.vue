8ce2ba0370968bec60f7a649faa4f3093c05cd91
Verge <jinxuyang@163.com>
2023-04-19 19:55:37 +0800

    init frontend

---
+++ frontend/smart-parking/App.vue
@@ -0,0 +1,32 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	@import './common/uni.css';
+	@import './static/customicons.css';
+	
+	uni-page-body {
+		background-color: #F5F5F5 !important;
+		min-height: 100% !important;
+		height: auto !important;
+	}
+	
+	.uni-top-window uni-tabbar .uni-tabbar {
+		background-color: #fff !important;
+	}
+	
+	.uni-app--showleftwindow .hideOnPc {
+		display: none !important;
+	}
+</style>
