fd6fcae3f11e6e7fca144ff1c40d41418a46e43f
Verge <jinxuyang@163.com>
2023-04-20 10:58:04 +0800

    test echarts

--- frontend/smart-parking/pages/index/index.vue
+++ frontend/smart-parking/pages/index/index.vue
@@ -1,24 +1,112 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+		<view><l-echart ref="chart" @finished="init"></l-echart></view>
 	</view>
 </template>
 
 <script>
+	import * as echarts from 'echarts/core';
+	import { TooltipComponent, GeoComponent } from 'echarts/components';
+	import { CanvasRenderer } from 'echarts/renderers';
+	
+	echarts.use([TooltipComponent, GeoComponent, CanvasRenderer]);
+	var takenSeatNames = ['a1'];
 	export default {
-		data() {
-			return {
-				title: 'Hello'
+	    data() {
+	        return {
+				option: {
+				    tooltip: {},
+				    geo: {
+				      map: 'flight-seats',
+				      roam: true,
+				      selectedMode: 'single',
+				      tooltip: {
+				        show: true
+				      },
+				      itemStyle: {
+				        color: '#fff'
+				      },
+				      emphasis: {
+				        itemStyle: {
+				          color: null,
+				          borderColor: 'green',
+				          borderWidth: 2
+				        },
+				        label: {
+				          show: false
+				        }
+				      },
+				      select: {
+				        itemStyle: {
+				          color: 'green'
+				        },
+				        label: {
+				          show: false,
+				          textBorderColor: '#fff',
+				          textBorderWidth: 2
+				        }
+				      },
+				      regions: this.makeTakenRegions(takenSeatNames)
+				    }
+				}
 			}
-		},
-		onLoad() {
-
-		},
+	    },
+	    
+	    mounted() {},
 		methods: {
-
+			init() {
+				this.$refs.chart.resize({width: 1000, height: 500})
+				this.$refs.chart.init(echarts, chart => {
+					uni.request({
+						url:"static/flight-seats.svg",
+						method:'GET',
+						success: (svg) => {
+							console.log(svg.data)
+							echarts.registerMap('flight-seats', {
+								svg: svg.data
+							})
+							chart.setOption(this.option);
+							chart.on('geoselectchanged', function (params) {
+								var selectedNames = params.allSelected[0].name.slice();
+										
+								// Remove taken seats.
+								for (var i = selectedNames.length - 1; i >= 0; i--) {
+									if (takenSeatNames.indexOf(selectedNames[i]) >= 0) {
+										selectedNames.splice(i, 1);
+									}
+								}
+										
+								console.log('selected', selectedNames);
+							});
+						}
+					})
+				});
+			},
+			makeTakenRegions(takenSeatNames) {
+				
+				var regions = [];
+				for (var i = 0; i < takenSeatNames.length; i++) {
+				  regions.push({
+					name: takenSeatNames[i],
+					silent: true,
+					itemStyle: {
+					  color: '#bf0e08'
+					},
+					emphasis: {
+					  itemStyle: {
+						borderColor: '#aaa',
+						borderWidth: 1
+					  }
+					},
+					select: {
+					  itemStyle: {
+						color: '#bf0e08'
+					  }
+					}
+				  });
+				}
+				return regions;
+			}
 		}
 	}
 </script>
